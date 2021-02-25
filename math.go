e4e214d4eee01a6793c86b1e75b38759a3c367a8
avisnakovs <alexander.vishnyakov@bidstack.com>
2021-02-25 21:29:57 +0200

    math

--- math.go
+++ math.go
@@ -1,5 +1,36 @@
 package main
 
-func CalculateTrafficRatio(street Street, carCount int) float64 {
+import "math"
+
+func CalculateStreetRatio(street Street, carCount int) float64 {
 	return float64(carCount) / float64(street.length)
+}
+
+func CalculateTrafficRate(carCount map[Street]int) map[Street]float64 {
+	result := make(map[Street]float64)
+	for street, count := range carCount {
+		result[street] = CalculateStreetRatio(street, count)
+	}
+	return result
+}
+
+func NormalizeRatio(ratios map[Street]float64) map[Street]int {
+	min := math.MaxFloat64
+	result := make(map[Street]int)
+	for _, ratio := range ratios {
+		if min > ratio {
+			min = ratio
+		}
+	}
+	for street, ratio := range ratios {
+		seconds := int(ratio / min)
+		if seconds < 1 {
+			seconds = 1
+		}
+		if seconds > 5 {
+			seconds = 5
+		}
+		result[street] = seconds
+	}
+	return result
 }
--- math_test.go
+++ math_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCalculateTrafficRatio(t *testing.T) {
 	tests := []struct {
@@ -15,9 +19,38 @@ func TestCalculateTrafficRatio(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := CalculateTrafficRatio(tt.street, tt.carCount); got != tt.want {
-				t.Errorf("CalculateTrafficRatio() = %v, want %v", got, tt.want)
+			if got := CalculateStreetRatio(tt.street, tt.carCount); got != tt.want {
+				t.Errorf("CalculateStreetRatio() = %v, want %v", got, tt.want)
 			}
 		})
 	}
+}
+
+func TestNormalizeRatio(t *testing.T) {
+	t.Run("test", func(t *testing.T) {
+		street1 := Street{title: "1", length: 2}
+		street2 := Street{title: "2", length: 3}
+		street3 := Street{title: "3", length: 6}
+		ratios := map[Street]float64{street1: 2, street2: 6, street3: 3}
+
+		got := NormalizeRatio(ratios)
+		assert.Equal(t, 1, got[street1])
+		assert.Equal(t, 3, got[street2])
+		assert.Equal(t, 1, got[street3])
+	})
+}
+
+func TestCalculateTrafficRate(t *testing.T) {
+
+	t.Run("test", func(t *testing.T) {
+		street1 := Street{title: "1", length: 2}
+		street2 := Street{title: "2", length: 3}
+		street3 := Street{title: "3", length: 6}
+		ratios := map[Street]int{street1: 2, street2: 6, street3: 3}
+
+		got := CalculateTrafficRate(ratios)
+		assert.Equal(t, 1.0, got[street1])
+		assert.Equal(t, 2.0, got[street2])
+		assert.Equal(t, .5, got[street3])
+	})
 }
